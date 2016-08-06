3b0f577170fc11a8de1b9a98c03ead31e9393459
amelius15 <anthonylobko@gmail.com>
2016-08-06 18:54:39 -0400

    added acquire data script

--- acquire_data.py
+++ acquire_data.py
@@ -13,13 +13,21 @@ WIDTH = 2
 CHANNELS = 2
 RATE = 44100
 
+max_points = 1000
+
+index = 1
+
+data_set = None
+
+label = 'NULL'
+
 p = pyaudio.PyAudio()
 
 def callback(in_data, frame_count, time_info, status):
     data = np.fromstring(in_data, 'Float32')
-    # print(len(data))
+    global index
+    global data_set
 
-    # data has NaNs in it; filter them out
     data_i = np.arange(len(data))
     mask = np.isfinite(data)
     filtered_data = np.interp(data_i, data_i[mask], data[mask])
@@ -27,12 +35,16 @@ def callback(in_data, frame_count, time_info, status):
     fft_data = np.fft.fft(filtered_data)
     real_fft = np.absolute(fft_data)
     real_fft /= np.max(np.abs(real_fft),axis=0)
-    # print(real_fft)
-    if True:
+    if data_set is not None:
+        data_set = np.vstack((data_set, real_fft))
+    else:
+        data_set = real_fft
+    index += 1
+    if False:
         return (in_data, pyaudio.paContinue)
     else:
         return (np.zeros(len(data)), pyaudio.paContinue)
-
+time.sleep(1)
 stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=CHANNELS,
                 rate=RATE,
@@ -40,12 +52,11 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
                 output=True,
                 stream_callback=callback,
                 frames_per_buffer=FRAMESIZE)
-# print (stream.format)
 stream.start_stream()
 
-while stream.is_active():
-    time.sleep(0.01)
-
+while stream.is_active() and index<max_points:
+    time.sleep(0.001)
+np.save(str(max_points) + '_' + str(RATE) + '_' + str(FRAMESIZE) + '_' + label, data_set)
 stream.stop_stream()
 stream.close()
 
