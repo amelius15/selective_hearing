3f99c7377d5ad392f964eb62e9db4bf810a01fcc
amelius15 <anthonylobko@gmail.com>
2016-08-07 13:22:34 -0400

    fixed app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, send_from_directory, jsonify
+from flask import Flask, request, send_from_directory, jsonify, send_file, render_template
 import json
+import logging
+import os, sys
+logging.basicConfig(
+            format="%(message)s",
+            level=logging.DEBUG,
+            stream=sys.stdout)
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
 @app.route('/static/<path:path>')
-def send_js(path):
+def send_file(path):
     return send_from_directory('static', path)
+
+@app.route('/')
+def send_index():
+    return render_template('index.html')
 
 @app.route('/state', methods=['GET', 'POST'])
 def login():
